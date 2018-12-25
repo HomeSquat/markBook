1ed3a62479b553102aa32b8ec8cabb01d082c26f
dongdongjie <384168902@qq.com>
2018-12-25 23:40:36 +0800

    网页标题栏图标

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
   description: '程序猿',
   // 注入到当前页面的 HTML <head> 中的标签
   head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
+    ['link', { rel: 'icon', href: '/favicon.png' }], // 增加一个自定义的 favicon(网页标签的图标)
   ],
   base: '/book/', // 这是部署到github相关的配置 下面会讲
   markdown: {
